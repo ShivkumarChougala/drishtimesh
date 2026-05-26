87d11fedc4d10256412abee6d7e526fe4776b643
ShivkumarChougala <shivakumarchougala9@gmail.com>
2026-05-26 22:48:58 +0530

    Polish frontend configuration and deployment styling

--- relay-api/app/main.py
+++ relay-api/app/main.py
@@ -23,6 +23,8 @@ app.add_middleware(
 	"http://localhost:5173",
         "http://192.168.31.212:5173",
         "http://127.0.0.1:5173",
+	"https://drishtimesh.thechougala.in",
+	"https://mesh.thechougala.in",
         "http://139.84.172.22:5173",
     ],
     allow_credentials=True,
